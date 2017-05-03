b504afd354a77df4c0dc779d2bb18b53a1af312a
Matt Parsons <parsonsmatt@gmail.com>
2017-05-02 22:47:44 -0600

    Add Aeson instances

--- src/Data/HashRecord/Internal.hs
+++ src/Data/HashRecord/Internal.hs
@@ -26,20 +26,26 @@ changes in here will /not/ be reflected in the major API version.
 -}
 module Data.HashRecord.Internal where
 
+import           Data.Aeson
+import           Data.Aeson.Types
 import           Data.Dynamic        (Dynamic, Proxy (..), Typeable,
                                       fromDynamic, toDyn)
 import           Data.HashMap.Strict (HashMap)
 import qualified Data.HashMap.Strict as Map
 import           Data.Maybe          (fromMaybe)
 import           Data.Monoid         ((<>))
+import qualified Data.Text           as Text
 import           GHC.TypeLits        (CmpSymbol, ErrorMessage (..), KnownSymbol,
                                       Symbol, TypeError, symbolVal)
 
 import           Prelude             hiding (lookup)
 
 -- | A 'HashRecord' is an extensible record, containing values of
--- potentially different types and indexed by strings. The
-newtype HashRecord (k :: [*]) = HashRecord { getHashRecord :: HashMap String Dynamic }
+-- potentially different types and indexed by strings.
+newtype HashRecord (k :: [*])
+    = HashRecord
+    { getHashRecord :: HashMap String Dynamic
+    }
 
 -- | The 'Record' type synonym is convenient for making type aliases for
 -- records. It sorts the keys and values you provide to it. You should
@@ -102,9 +108,53 @@ instance
             ix rec = unsafeLookup @k "HashRecord.==" rec :: v
          in this && rest
 
+instance ToPairs (HashRecord xs) => ToJSON (HashRecord xs) where
+    toJSON = object . toPairs
+
+-- | An auxiliary helper class to make it easier to define the 'ToJSON' class
+-- for 'HashRecord'.
+class ToPairs a where
+    toPairs :: a -> [Pair]
+
+-- | This is another example of inductive type class definitions. The base case
+-- is the empty map, which is just the empty list.
+instance ToPairs (HashRecord '[]) where
+    toPairs _ = []
+
+-- | To define the inductive step, we require that that the @k@ symbol is
+-- a 'KnownSymbol' to GHC, that we can convert the @v@ value 'ToJSON', and that
+-- the rest of the 'HashRecord' can be converted 'ToPairs'.
+instance
+    ( MapEntry k v
+    , ToJSON v
+    , ToPairs (HashRecord xs)
+    ) => ToPairs (HashRecord (k =: v ': xs)) where
+    toPairs rec@(HashRecord record) =
+        let this = Text.pack (symbolVal (Proxy @k)) .= toJSON (lookup @k rec)
+            rest = toPairs (HashRecord record :: HashRecord xs)
+         in this : rest
+
+instance FromJSON (HashRecord '[]) where
+    parseJSON = withObject "HashRecord" $ \_ -> pure (HashRecord mempty)
+
+instance
+    ( FromJSON v
+    , MapEntry k v
+    , FromJSON (HashRecord xs)
+    ) => FromJSON (HashRecord (k =: v ': xs)) where
+    parseJSON = withObject "HashRecord" $ \o -> do
+        let key = symbolVal (Proxy @k)
+        val :: v <- o .: Text.pack key
+        HashRecord rest :: HashRecord xs <- parseJSON (Object o)
+        pure (HashRecord (Map.singleton key (toDyn val) <> rest))
+
 -- | Construct an empty 'HashRecord'.
 empty :: HashRecord '[]
 empty = HashRecord mempty
+
+-- | Construct a singleton 'HashRecord'.
+singleton :: forall key val. MapEntry key val => val -> HashRecord '[key =: val]
+singleton v = insert @key v empty
 
 -- | '=:' is a type that pairs type level 'String's (aka 'Symbol's) with
 -- a value at the term level.
@@ -116,8 +166,9 @@ insert
     => val
     -> HashRecord keys
     -> HashRecord (InsertSorted key val keys)
-insert val (HashRecord record) =
-     HashRecord (Map.insert (symbolVal (Proxy @key)) (toDyn val) record)
+insert val = HashRecord
+    . Map.insert (symbolVal (Proxy @key)) (toDyn val)
+    . getHashRecord
 
 -- | This type family looks up the key in the given list of key value
 -- pairs, failing if the value is not what was anticipated or if the value
@@ -212,13 +263,21 @@ delete
     )
     => HashRecord keys
     -> HashRecord (Remove key keys)
-delete (HashRecord record) =
-    HashRecord (Map.delete (symbolVal (Proxy @key)) record)
+delete = HashRecord
+    . Map.delete (symbolVal (Proxy @key))
+    . getHashRecord
 
 -- | The type signature is inferred. Hooray!
 testMap :: HashRecord '["foo" =: Char]
 testMap = insert @"foo" 'a' empty
 
+-- | This updates the value stored in the 'HashRecord', potentially
+-- changing it's type. Unlike 'Data.HashMap.Strict.update', this function
+-- does not allow you to remove the entry if present. To remove an entry,
+-- you'll need to use 'delete'.
+--
+-- >>> lookup @"foo" (update @"foo" succ (insert @"foo" 'a' empty))
+-- 'b'
 update
     :: forall key val1 val2 keys.
     ( MapEntry key val1
@@ -228,8 +287,9 @@ update
     => (val1 -> val2)
     -> HashRecord keys
     -> HashRecord (UpdateAt key val2 keys)
-update f (HashRecord record) =
-    HashRecord (Map.update k (symbolVal (Proxy @key)) record)
+update f = HashRecord
+    . Map.update k (symbolVal (Proxy @key))
+    . getHashRecord
   where
     k = Just
         . toDyn
